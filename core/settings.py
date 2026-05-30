cd7eabd4b2e7e2be3fc7bae57805d9838bc6652e
Digamber Jha <kumardigamberjha@gmail.com>
2026-05-30 17:46:55 +0530

    Added uv lock and fixed pyproject.toml

--- core/settings.py
+++ core/settings.py
@@ -154,6 +154,9 @@ os.makedirs(BASE_DIR / 'media', exist_ok=True)
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
